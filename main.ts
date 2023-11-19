ce7b2873c1a89040f126f150dc741c114116af11
MuhammadShariq368 <muhammadshariq368@gmail.com>
2023-11-19 17:09:48 +0500

    new code

--- main.ts
+++ main.ts
@@ -1,8 +1,43 @@
-// Question 1//
+// Question 2//
 console.log("Hello Shariq would you like to learn some Phyton today?")
-// Question 2 //
-const PersonName = "Muhammad Shariq"
+// Question 3 //
+const Personname = "Muhammad Shariq";
 
-console.log(`Person name in lowercase: ${personName.toLowerCase()}`);
-console.log(`Person name in uppercase: ${personName.toUpperCase()}`);
-console.log(`Person name in titlecase: ${personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()}`);
+console.log(`Person name in lowercase: ${Personname.toLowerCase()}`);
+console.log(`Person name in uppercase: ${Personname.toUpperCase()}`);
+console.log(`Person name in titlecase: ${Personname.charAt(0).toUpperCase() + Personname.slice(1).toLowerCase()}`);
+// Question 4//
+let quote = "A person who never made a mistake never tried anything new.";
+let author = "Albert Einstein";
+
+console .log(`${author} once said "${quote}"`)
+
+// Question 5//
+let famous_person = "Albert Einstein";
+let Quote = "A person who never made a mistake never tried anything new.";
+let message = `${famous_person} once said "${Quote}"`
+
+ console.log(message);
+
+ // Question 6 //
+
+
+
+
+
+
+// Question 7 //
+// Addition //
+console.log(5+3);
+//Subtraction
+console.log(10 - 2);
+// Multiplication
+console.log(2 * 4);
+// Division
+console.log(16 / 2);
+
+// Question 9//
+console.log(8);
+console.log(8);
+console.log(8);
+console.log(8);
